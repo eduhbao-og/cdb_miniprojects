dfec15e41fcaef2e9966ef81a97761bd38879400
Vicente5000 <goncasmendonca42@gmail.com>
2026-03-05 19:40:57 +0000

    attributes for coin and nft

--- library/src/main/java/dcb/dti/Coin.java
+++ library/src/main/java/dcb/dti/Coin.java
@@ -1,6 +1,10 @@
 package dcb.dti;
 
 public class Coin {
+     /*ID: an integer (long) with the coin ID (no two coins can have the same id)
+    o Owner: an integer (int) with the id of the client owning the coin
+    o Value: a real number (long) with the value of the coin*/
     long id;
-    //...
+    int owner;
+    long value;
 }
--- library/src/main/java/dcb/dti/NFT.java
+++ library/src/main/java/dcb/dti/NFT.java
@@ -1,6 +1,14 @@
 package dcb.dti;
 
 public class NFT {
+    /*ID: an integer (long) with the NFT id (no two NFTs can have the same id)
+    o Owner: an integer (int) with the id of the client owning the NFT
+    o Name: the name (a String) of the NFT
+    o URI: the URI (a String, typically a URL) of the NFT
+    o Value: a real number (long) with the value of the NFT*/
     long id;
-    //...
+    int owner;
+    String name;
+    String uri;
+    long value;
 }
