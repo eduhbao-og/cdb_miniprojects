d7e723fa1dbd3aa3225d3328d5cf039da1fecab1
eduhbao-og <eduhbao@duck.com>
2026-05-25 11:56:49 +0100

    add: rudimentary nft contract

--- project_3/contracts/NFTContract.sol
+++ project_3/contracts/NFTContract.sol
@@ -1,3 +1,26 @@
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.28;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTContract is ERC721 {
+
+    uint256 private tokenIdCounter;
+
+    constructor() payable ERC721("Simple NFT", "SNFT"){}
+
+    function mint() external payable {
+        require(msg.value == mintPrice, "Wrong value");
+        tokenIdCounter += 1;
+        uint256 tokenId = tokenIdCounter;
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function burn() external {}
+    function buy(address seller) external payable {}
+    function sell(uint256 tokenId, uint256 price) external {}
+    function auction(uint256 tokenId, uint256 minimumPrice, uint256 duration) external {}
+
+    event auctionCreated(address seller, uint256 tokenId, uint256 minimumPrice, uint256 duration);
+    event auctionEnded(address seller, address buyer, uint256 tokenId, uint256 finalPrice);
+    
+}
