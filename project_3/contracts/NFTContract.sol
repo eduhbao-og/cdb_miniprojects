4ff0f2319017af22b546f23eb7a30a6b643b48e0
eduhbao-og <eduhbao@duck.com>
2026-05-25 15:15:25 +0100

    add: basic burn function

--- project_3/contracts/NFTContract.sol
+++ project_3/contracts/NFTContract.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.28;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTContract is ERC721 {
+contract NFTContract is ERC721, Ownable {
 
     uint256 private tokenIdCounter;
 
@@ -15,12 +16,9 @@ contract NFTContract is ERC721 {
         _safeMint(msg.sender, tokenId);
     }
 
-    function burn() external {}
-    function buy(address seller) external payable {}
-    function sell(uint256 tokenId, uint256 price) external {}
-    function auction(uint256 tokenId, uint256 minimumPrice, uint256 duration) external {}
-
-    event auctionCreated(address seller, uint256 tokenId, uint256 minimumPrice, uint256 duration);
-    event auctionEnded(address seller, address buyer, uint256 tokenId, uint256 finalPrice);
-    
+    function burn(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+        _burn(tokenId);
+    }
+   
 }
