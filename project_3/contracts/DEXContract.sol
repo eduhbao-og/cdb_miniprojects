9fc70020ea97f1cdb294e08787195881374d376d
Vicente5000 <goncasmendonca42@gmail.com>
2026-05-26 15:36:00 +0100

    ADD: DEX contract

--- project_3/contracts/DEXContract.sol
+++ project_3/contracts/DEXContract.sol
@@ -1,21 +1,34 @@
 pragma solidity ^0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DEXContract is ERC20 {
+contract DEXContract is ERC20, Ownable {
 
-    uint256 dexSwapRate;
+    uint256 public dexSwapRate;
 
-    constructor(uint256 swapRate) ERC20("DEX", "DEX") {
+    constructor(uint256 swapRate) ERC20("DEX", "DEX") Ownable(msg.sender) {
+        require(swapRate > 0, "Swap rate must be positive");
+        dexSwapRate = swapRate;
         _mint(address(this), 10**18);
+    }
+
+    function DEXtoETH(uint256 dexAmount) public view returns (uint256) {
+        return (dexAmount * 1 ether) / dexSwapRate;
+    }
+
+    function ETHtoDEX(uint256 ethAmount) public view returns (uint256) {
+        return (ethAmount * dexSwapRate) / 1 ether;
+    }
+
+    function getDexBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
+    }
+
+    function setDexSwapRate(uint256 swapRate) external onlyOwner {
+        require(swapRate > 0, "Swap rate must be positive");
         dexSwapRate = swapRate;
     }
 
-    function DEXtoETH(uint dexAmount) view private returns (uint) {
-        return (dexAmount * 10**18)/dexSwapRate;
-    }
-
-    function ETHtoDEX(uint ethAmount) view private returns (uint) {
-        return (ethAmount * dexSwapRate)/10**18;
-    }
+    receive() external payable {}
 }
